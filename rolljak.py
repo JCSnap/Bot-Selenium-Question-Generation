ab8129989c5d99f34560dfaaebae17b4cc1ee36b
JCSnap <e0970167@u.nus.edu>
2023-07-03 22:52:52 +0800

    parse and insert into excel

--- rolljak.py
+++ rolljak.py
@@ -1,15 +1,18 @@
 import os
+import os.path
 from flask import Flask, request, jsonify
 import openai
 import constants
-import requests
-import json
-import time
 import re
+import constants
+import pandas as pd
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
-
+file_path = "~/Downloads/excel.xlsx"
 MODEL = "gpt-4"
+
+# If the file exists, read the existing data, otherwise create an empty DataFrame
+df = pd.read_excel(file_path)
 
 number_of_questions = constants.number_of_questions
 grade = constants.grade
@@ -23,6 +26,13 @@ Student educational level: {grade}
 Number of questions: 1
 Question type(s): MCQ
 """
+
+topic = constants.topic
+grade = constants.grade
+subject = constants.subject
+generator = "Rolljak"
+type_of_question = "Multiple Choice"
+number_of_correct_answer = "1"
 
 # QUESTION SPLIT
 completion = openai.ChatCompletion.create(
@@ -43,32 +53,70 @@ subtopics = [re.sub(r'^\d+\. ', '', line)
 
 prompt = SETTINGS + "\n"
 
-for i in range(1):
-    prompt = SETTINGS + "\n" + subtopics[i] + constants.MCQ_FORMAT + "\n"
-    completion = openai.ChatCompletion.create(
-        model=MODEL,
-        messages=[
-            {"role": "system", "content": constants.QUESTION_GENERATION_INSTRUCTION},
-            {"role": "user", "content": prompt}
-        ]
-    )
-    question = completion.choices[0].message.content
-    print(question)
+for i in range(len(subtopics)):
+    try:
+        prompt = SETTINGS + "\n" + subtopics[i] + constants.MCQ_FORMAT + "\n"
+        completion = openai.ChatCompletion.create(
+            model=MODEL,
+            messages=[
+                {"role": "system", "content": constants.QUESTION_GENERATION_INSTRUCTION},
+                {"role": "user", "content": prompt}
+            ]
+        )
+        question = completion.choices[0].message.content
+        print(question)
 
-    # Split the text by double newlines to get each section separately
-    sections = question.split("\n\n")
+        subtopic_start = question.index("Subtopic:")
+        question_start = question.index("Question:")
+        options_start = question.index("Options:")
+        answer_start = question.index("Answer:")
 
-    # Parse the question
-    question = sections[1].replace("Question: ", "")
+        # Parse the subtopic
+        subtopic = question[subtopic_start +
+                            len("Subtopic:"): question_start].strip()
 
-    # Parse the options
-    options_text = sections[2].replace("Options:\n", "")
-    options = re.findall(r'\. (.*)', options_text)
+        # Parse the question
+        question_mcq = question[question_start +
+                                len("Question:"): options_start].strip()
 
-    # Parse the answer
-    answer_text = sections[3].replace("Answer: ", "")
-    answer = ord(answer_text) - ord('A')  # convert from letter to index
+        # Parse the options
+        options_text = question[options_start +
+                                len("Options:"): answer_start].strip()
+        options = re.findall(r'\. (.*)', options_text)
 
-    print("Question:", question)
-    print("Options:", options)
-    print("Answer:", answer)
+        # Parse the answer
+        answer_text = question[answer_start:].strip()
+        print(question_mcq)
+        print(options_text)
+        print(answer_text)
+        match = re.search(r'Answer: (\w)', answer_text)
+        answer_letter = match.group(1)
+        answer = ord(answer_letter) - ord('A')
+
+        # Get the correct option as a string and the incorrect options
+        correct_option = options[answer]
+        incorrect_options = options[:answer] + options[answer+1:]
+
+        # Create a new row for this question
+        new_row = {
+            'Topic': topic,
+            'Grade': grade,
+            'Subject': subject,
+            'Generator': generator,
+            "Type of question": type_of_question,
+            'Question': question_mcq,
+            'Answer': number_of_correct_answer,
+            'Content': correct_option,
+            'Unnamed: 4': incorrect_options[0],
+            'Unnamed: 5': incorrect_options[1],
+            'Unnamed: 6': incorrect_options[2]
+        }
+
+        # Append the new row to the DataFrame
+        df = df.append(new_row, ignore_index=True)
+    except:
+        print("Error")
+        continue
+
+# Write the entire DataFrame to excel
+df.to_excel(file_path, index=False)
