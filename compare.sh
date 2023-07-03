e38cd0f74e97696620e5371baf0dce80ecc4f335
JCSnap <e0970167@u.nus.edu>
2023-07-03 22:52:30 +0800

    created all scripts to run

--- compare.sh
+++ compare.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-# Generate question using QuizWizard
+echo "Running quizwizard.py"
 python3 quizwizard.py
 
+echo "Running add_topic_level.py"
+python3 add_topic_level.py
+
+echo "Running rolljak.py"
+python3 rolljak.py
+
+echo "Running append.py"
+python3 append.py
+
+echo "Removing excel"
+rm ~/Downloads/excel.xlsx
